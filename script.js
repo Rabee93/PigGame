7e17e3cb7fa9a1480cac2f0e1fea95f2a2edc568
Rabee Mghrabi <learningprogramming2019@gmail.com>
2021-03-04 22:29:04 +0000

    added switching players functionality

--- script.js
+++ script.js
@@ -4,6 +4,15 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const newGame = document.querySelector('.btn--new');
+const rollDiceBtn = document.querySelector('.btn--roll');
+const holdBtn = document.querySelector('.btn--hold');
+const currentScore0 = document.getElementById('current--0');
+const currentScore1 = document.getElementById('current--1');
+
+let score0 = 0
+let score1 = 0
+
 
 // starting conditions
 score0El.textContent = 0;
@@ -14,6 +23,19 @@ diceEl.classList.add("hidden")
 
 rollDiceBtn.addEventListener("click", function(){
     const diceNum = Math.trunc(Math.random()*6) + 1;
+    console.log(diceNum)
     diceEl.classList.remove("hidden")
     diceEl.src = `dice-${diceNum}.png`
+
+    if(diceNum != 1){
+        score0 += diceNum
+        currentScore0.textContent = score0
+
+
+    }else {
+        score0 = 0
+
+
+
+    }
 })
