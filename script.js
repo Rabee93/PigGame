ac29a7f151be5c9e9ab955ab56cfe487310db958
Rabee Mghrabi <learningprogramming2019@gmail.com>
2021-03-09 20:40:27 +0000

    added playing boolean to stop the game when a player wins

--- script.js
+++ script.js
@@ -14,29 +14,32 @@ const currentScore1 = document.getElementById('current--1');
 
 let activePlayer = 0
 let currentScore = 0
+let playing = true
 
 // starting conditions
 let score = [0,0]
 score0El.textContent = 0;
 score1El.textContent = 0;
+
 diceEl.classList.add("hidden")
 
 // rolling dice functionality
 
 rollDiceBtn.addEventListener("click", function(){
+    if(playing){
     const diceNum = Math.trunc(Math.random()*6) + 1;
     console.log(diceNum)
     diceEl.classList.remove("hidden")
     diceEl.src = `dice-${diceNum}.png`
 
     if(diceNum != 1){
-        score0 += diceNum
-        document.getElementById(`current--${activePlayer}`).textContent = score0
+        currentScore += diceNum
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore
 
 
       }else {
-              score0 = 0
-              document.getElementById(`current--${activePlayer}`).textContent = score0
+              currentScore = 0
+              document.getElementById(`current--${activePlayer}`).textContent = 0
               activePlayer = activePlayer === 0 ? 1:0
 
               if(activePlayer === 1){
@@ -50,35 +53,36 @@ rollDiceBtn.addEventListener("click", function(){
 
 
 
+
           }
+        }
       })
+  holdBtn.addEventListener("click", function(){
+    // 1. add current score to score
+    if(playing){
+    score[activePlayer] += currentScore
+    document.getElementById(`score--${activePlayer}`).textContent= score[activePlayer]
+    if(score[activePlayer] >= 10){
+        console.log("hi")
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+        playing = false
 
-      holdBtn.addEventListener("click", function(){
-        score[activePlayer] += currentScore
-          document.getElementById(`score--${activePlayer}`).textContent= score[activePlayer]
-          if(score[activePlayer] >= 100){
-              console.log("hi")
-              document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
-              document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
-              diceEl.classList.add('hidden')
-              holdBtn.classList.add('hidden')
-              rollDiceBtn.classList.add('hidden')
+    }
+    currentScore = 0
+    document.getElementById(`current--${activePlayer}`).textContent = 0
+    activePlayer = activePlayer === 0 ? 1:0
+    if(activePlayer === 1){
+        player0.classList.remove('player--active')
+        player1.classList.add('player--active')
 
-          }
-          currentScore = 0
-          document.getElementById(`current--${activePlayer}`).textContent = 0
-          activePlayer = activePlayer === 0 ? 1:0
-          if(activePlayer === 1){
-              player0.classList.remove('player--active')
-              player1.classList.add('player--active')
+    } else {
+        player0.classList.add('player--active')
+        player1.classList.remove('player--active')
+    }
 
-          } else {
-              player0.classList.add('player--active')
-              player1.classList.remove('player--active')
-          }
+    // 2. if player reaches 100 he wins
 
-          // 2. if player reaches 100 he wins
-
-          // 3. if player is less than 100 then switch players
-
-      })
+    // 3. if player is less than 100 then switch players
+    }
+})
